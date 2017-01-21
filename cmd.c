c2efd86f67d1efadc55267777c90223a4c3850da
MaestracciDamien <damien.maestracci@etu.univ-nantes.fr>
2017-01-21 22:36:27 +0100

    args

--- cmd.c
+++ cmd.c
@@ -17,7 +17,7 @@ void print_members_args(cmd *c){
     {
       for (j=0; j< c->nb_members_args[i]; j++ )
       {
-        printf("cmd_members_args[%d][%d]=\"%s\"\n",i,j,cmd_members_args[i][j] );
+        printf("cmd_members_args[%d][%d]=\"%s\"\n",i,j,c->cmd_members_args[i][j] );
       }
     }
 }
@@ -63,26 +63,27 @@ void free_redirection(cmd *c){
 //Remplit les champs cmd_args et nb_args_membres
 void parse_members_args(cmd *c){
 	int i=0;
-    int cpt=0;
-    c->cmd_members_args =  NULL;
-    c->nb_members_args =NULL;
-    while (cpt < (c->nb_cmd_members)){
-  		int j=0;
-	    char *token = strtok(c->cmd_members[cpt]," ");
-	    while (token!=NULL && strcmp(token,"<")!=0 && strcmp(token,">")!=0 && strcmp(token,">>")!=0){
-	    	int length = strlen(token);
-	    	printf("%s\n", token );
-	    	c->cmd_members_args=(char***)realloc(c->cmd_members_args, sizeof(char*) * (i+1));
-	    	c->nb_members_args=(unsigned int*)realloc(c->nb_members_args, sizeof(int*) * (i+1));
-	    	memcpy(c->cmd_members_args[cpt][j],token,length);
-  		    j++;
-  		    token = strtok(NULL," ");
-  		    i++;
-	    }
-	    c->nb_members_args[cpt]=j-1;
-	    c->cmd_members_args=(char***)realloc(c->cmd_members_args, sizeof(char*) * (i+1));
-	    memcpy(c->cmd_members_args[cpt][j],token,1);
-	    cpt++;
+    //int cpt=0;
+    c->cmd_members_args =  (char***) malloc (sizeof(char **)*c->nb_cmd_members);
+    c->nb_members_args = (unsigned int*) malloc (sizeof(int)*c->nb_cmd_members);
+    while (i < (c->nb_cmd_members)){
+    		int j=0;
+  	    char *token = strtok(c->cmd_members[i]," ");
+        c->cmd_members_args[i] = NULL;
+        c->nb_members_args[i] = NULL;
+  	    while (token!=NULL && strcmp(token,"<")!=0 && strcmp(token,">")!=0 && strcmp(token,">>")!=0)
+        {
+    	    	int length = strlen(token)+1;
+    	    	//printf("%s\n", token );
+            c->cmd_members_args[i]= (char ** ) realloc (c->cmd_members_args[i], sizeof(char *) *j+1);
+            c->cmd_members_args[i][j] = (char *) malloc (sizeof(char)*length);
+    	    	memcpy(c->cmd_members_args[i][j],token,length);
+      		    j++;
+      		    token = strtok(NULL," ");
+
+  	    }
+        c->nb_members_args[i] =j;
+        i++;
     }
 
 }
