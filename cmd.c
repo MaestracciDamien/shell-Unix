50520292565582fb7320db30240a7aab080ae897
Raphael LEPILLER <raphael.lepiller@gmail.com>
2017-01-21 20:16:22 +0100

    parse_members_args

--- cmd.c
+++ cmd.c
@@ -1,5 +1,8 @@
 #include "cmd.h"
 //Your includes come here
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 //Prints the contents of members_args to the console
 void print_members_args(cmd *c){
@@ -33,7 +36,21 @@ void free_redirection(cmd *c){
 
 //Remplit les champs cmd_args et nb_args_membres
 void parse_members_args(cmd *c){
-    //your implementation comes here
+    int cpt=0;
+    while (cpt < (c->nb_cmd_members)){
+  		int j=0;
+	    char *token = strtok(c->cmd_members[cpt]," ");
+	    while (token!=NULL){
+	    	int length = strlen(token);
+	    	printf("%s\n", token );
+	    	(char *) c->cmd_members_args[cpt][j]=(char*)malloc(sizeof(char)*length);
+	    	memcpy(c->cmd_members_args[cpt][j],token,length);
+  		    j++; 
+  		    token = strtok(NULL," "); 		    
+	    }
+	    cpt++;
+    }
+
 }
 
 //Remplit les champs initial_cmd, membres_cmd et nb_membres
